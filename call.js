a873c84632cafc2865e05fc8f6c4a0a468a36e33
yared-abera <yaredoabera7@gmail.com>
2024-07-11 11:51:45 +0300

    modified call

--- call.js
+++ call.js
@@ -1,10 +1,20 @@
 //call
+/*
+call method is used to change the "this" keyword of the function.
+
+Syntax of call method:
+
+function.call(thisArg, arg1, arg2, ...);
+call method is used for function outside the object to use this keyword AS REFERENCe of the provided object instead of global empty object.
+
+*/
 var person =
 {
   age: 20
+
 };
 
-let birthDay = function(years) {
+let birthDay = function (years) {
   this.age += years;
 };
 
